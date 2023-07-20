8c893f812434c0ae16161500e8dc5c2c85d0cc33
Tatiana Lagodich <t.lagodich@amarkets.com>
2023-07-20 13:09:48 +0400

    feat: rendering vdom

--- src/component.js
+++ src/component.js
@@ -1,16 +1,15 @@
-import { createReactivityObject } from "./proxy";
-import { VDom, parseToVDomString } from "./VDom";
-
-export class Component {
-    constructor({ template, data, watch, methods }) {
-        // нужно привязывать контекст через bind
-        this.template = template;
-        this.data = data();
-        this.data = createReactivityObject(this.data, watch.bind(this.data));
-        this.methods = methods;
-    }
-
-    render() {
-        return parseToVDomString(this.template(), this.data);
-    }
-}
+// import { createReactivityObject } from "./proxy";
+//
+// export class Component {
+//     constructor({ template, data, watch, methods }) {
+//         // нужно привязывать контекст через bind
+//         this.template = template;
+//         this.data = data();
+//         this.data = createReactivityObject(this.data, watch.bind(this.data));
+//         this.methods = methods;
+//     }
+//
+//     render() {
+//         return this.template(this.data, this.props);
+//     }
+// }
